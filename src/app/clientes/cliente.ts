464577bebe373602327432e047a0be89506eb163
Federico Pernas <federico.pernas@endava.com>
2021-01-26 18:06:57 -0300

    angular-project

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente {
+
+  id:Number = 0;
+  nombre:string = '';
+  apellido:string = '';
+  email:string = '';
+  createdAt:string = '';
+
+
+
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,5 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {nombre: 'Federico', apellido: 'Piernas',createdAt: 'today',email: 'some@some',id: 1}
+]
